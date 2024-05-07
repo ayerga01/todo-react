ff3eef4f54ae6588403ecfc5abe574d8fb9748dc
Alba Yerga Castells <ayerga@altech.es>
2024-05-07 15:58:29 +0200

    "npm i @tanstack/react-query-devtools"

--- todo-react/src/App.tsx
+++ todo-react/src/App.tsx
@@ -1,14 +1,22 @@
-import React, {FC, ReactElement} from 'react';
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import { customTheme } from './theme/customTheme';
-import { Dashboard } from './pages/dashboard/dashboard';
+import React, { FC, ReactElement } from "react";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { customTheme } from "./theme/customTheme";
+import { Dashboard } from "./pages/dashboard/dashboard";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const App:FC = (): ReactElement => {
+// Create a client
+const queryClient = new QueryClient();
+
+const App: FC = (): ReactElement => {
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
         <Dashboard />
-    </ThemeProvider>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
-}
+};
 export default App;
