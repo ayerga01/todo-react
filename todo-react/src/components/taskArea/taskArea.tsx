5de7918639131611f3f8c4f2b91a4f5824649412
Alba Yerga Castells <ayerga@altech.es>
2024-05-17 10:51:51 +0200

    chore: countTasks added in TaskArea

--- todo-react/src/components/taskArea/taskArea.tsx
+++ todo-react/src/components/taskArea/taskArea.tsx
@@ -8,6 +8,7 @@ import { sendApiRequest } from "../../helpers/sendApiRequest";
 import { ITaskApi } from "./interfaces/ITaskApi";
 import { Status } from "../createTaskForm/enums/Status";
 import { IUpdateTask } from "../createTaskForm/interfaces/IUpdateTask";
+import { countTasks } from "./helpers/countTasks";
 
 export const TaskArea: FC = (): ReactElement => {
   const { error, isLoading, data, refetch } = useQuery({
@@ -68,9 +69,18 @@ export const TaskArea: FC = (): ReactElement => {
           xs={12}
           mb={8}
         >
-          <TaskCounter />
-          <TaskCounter />
-          <TaskCounter />
+          <TaskCounter
+            count={data ? countTasks(data, Status.todo) : undefined}
+            status={Status.todo}
+          />
+          <TaskCounter
+            count={data ? countTasks(data, Status.inProgress) : undefined}
+            status={Status.inProgress}
+          />
+          <TaskCounter
+            count={data ? countTasks(data, Status.completed) : undefined}
+            status={Status.completed}
+          />
         </Grid>
         <Grid item display="flex" flexDirection="column" md={8} xs={10}>
           <>
